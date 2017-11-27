316d8159c81d45a832df6960d384f7119664b253
Unknown <parw1633@gmail.com>
2017-11-27 18:08:59 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss." << endl;
+  cout << "Duangsamon" << endl;
+  cout << "Puetsalung" << endl;
+  cout << "Parw" << endl;
+  cout << "women" << endl;
+  cout << "02/01/1999" << endl;
+  cout << "playgame" << endl;
+  cout << "Student" << endl;
   return 0;
 }
