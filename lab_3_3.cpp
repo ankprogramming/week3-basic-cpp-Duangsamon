681589ff0e852046406d12fd175ad84e995fd327
Unknown <parw1633@gmail.com>
2017-11-28 11:59:15 +0700

    ทำข้อ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main() {
-  
+
+    const float pi = 3.14315926536;
+    float r;
+    float h=15;
+    float area;
+    float around;
+    float cylinder=15;
+
+    cout << "Please enter radius : ";
+    cin >> r;
+    area = pi * r * r;
+    cout << "Area of Circle : " << area << endl;
+
+    around = 2 * pi * r;
+    cout << "Area of Circle : " << around << endl;
+
+    cylinder = pi * r * h;
+    cout << "Area of Circle : " << cylinder << endl;
+
   return 0;
 }
